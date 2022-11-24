0748bc05a7784d44505d898c2cbc86531a0bfff2
Harshita sharma <117646394+harshitabca@users.noreply.github.com>
2022-11-24 16:49:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./header";
+
+const ul = ul.createElement("ul");
+const li = li.createElement("li");
+li.appendChild(li);
+li.appendChild(document.createTextNode("Hey"));
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div classname="dummy"></div>
+    </div>
+  );
+}
+
+export default App;
